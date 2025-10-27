d0f431adb9c65d89ff959d12c08364ffb1450b58
Dubaiplatform <dubaiplatformdm@gmail.com>
2025-10-27 15:13:08 +0400

    vite

--- drep-ui/vite.config.ts
+++ drep-ui/vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths() // <-- this enables TypeScript path aliases
+    tsconfigPaths() // enables TS path aliases
   ],
   resolve: {
     alias: {
-      "@": "/src", // optional, tsconfigPaths handles most cases
+      "@": path.resolve(__dirname, "src"), // <-- correct
     },
   },
 });
