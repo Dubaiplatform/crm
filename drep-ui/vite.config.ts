f9ba153fb6a24bc1d4907b57161dd79886905895
Dubaiplatform <dubaiplatformdm@gmail.com>
2025-10-27 15:45:13 +0400

    tesat

--- drep-ui/vite.config.ts
+++ drep-ui/vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths() // enables TS path aliases from tsconfig.json
+    tsconfigPaths() 
   ],
 });
